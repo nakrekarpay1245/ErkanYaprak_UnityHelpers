1f37585ba51e47434472bd13d749f7e4578c512a
nakrekarpay1245 <rknyprk2001@gmail.com>
2024-08-21 12:42:58 +0300

    feat: Added ParticleData script
    ParticleData Class:
    
    Purpose:
    - To store data for a specific type of particle effect.
    
    Properties:
    - ParticleName: The name of the particle effect.
    - ParticleCount: The number of particles to be generated.
    - _particleSystemList: List of particle system prefabs.
    
    Methods:
    - ParticleSystem: Property that returns a random particle system from the list.
    - Constructor: Configures the particle systems, count, and name.

---
+++ ErkanYaprak_UnityHelpers/Assets/_Game/Scripts/_helpers/Particle/ParticleData.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Game.Scripts._helpers
+{
+    /// <summary>
+    /// Stores data for a specific particle type, including its name, count, and associated particle systems.
+    /// </summary>
+    [System.Serializable]
+    public class ParticleData
+    {
+        [Header("Particle Settings")]
+        [Tooltip("The name of the particle effect.")]
+        public string ParticleName;
+
+        [Tooltip("The number of particles to be generated.")]
+        public int ParticleCount;
+
+        [Tooltip("The list of particle system prefabs.")]
+        [SerializeField]
+        private List<ParticleSystem> _particleSystemList;
+
+        /// <summary>
+        /// Gets a random particle system from the list of particle systems.
+        /// </summary>
+        public ParticleSystem ParticleSystem => _particleSystemList[Random.Range(0, _particleSystemList.Count)];
+
+        /// <summary>
+        /// Initializes a new instance of the ParticleData class.
+        /// </summary>
+        /// <param name="particleSystems">The list of particle system prefabs.</param>
+        /// <param name="count">The number of particles to be generated.</param>
+        /// <param name="name">The name of the particle effect.</param>
+        public ParticleData(List<ParticleSystem> particleSystems, int count, string name)
+        {
+            _particleSystemList = particleSystems ?? throw new System.ArgumentNullException(nameof(particleSystems));
+            ParticleCount = count > 0 ? count : throw new System.ArgumentOutOfRangeException(nameof(count), "Particle count must be greater than zero.");
+            ParticleName = name ?? throw new System.ArgumentNullException(nameof(name));
+        }
+    }
+}
