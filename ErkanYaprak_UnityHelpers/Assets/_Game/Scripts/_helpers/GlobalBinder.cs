69f01dcd184ad68c06dfeb497615956629ad8251
nakrekarpay1245 <rknyprk2001@gmail.com>
2024-08-21 19:26:07 +0300

    feat: Added GlobalBinder.cs
    GlobalBinder Class:
    
    Acts as a central hub for accessing various managers in the game.
    Inherits from MonoSingleton, ensuring a single instance across the game.
    Managers:
    
    AudioManager: Manages audio functionalities, such as playing sounds and handling music.
    ParticleManager: Manages particle effects used throughout the game.
    TimeManager: Manages time-related functions, including countdowns and timers.
    Public Access:
    
    Provides public access to the AudioManager, ParticleManager, and TimeManager instances through properties.

---
+++ ErkanYaprak_UnityHelpers/Assets/_Game/Scripts/_helpers/GlobalBinder.cs
@@ -0,0 +1,38 @@
+using _Game.Scripts._helpers.Audios;
+using _Game.Scripts._helpers.Particles;
+using UnityEngine;
+
+namespace _Game.Scripts._helpers
+{
+    /// <summary>
+    /// Acts as a central hub for accessing various managers in the game.
+    /// Inherits from MonoSingleton to ensure a single instance across the game.
+    /// </summary>
+    public class GlobalBinder : MonoSingleton<GlobalBinder>
+    {
+        [Header("Managers")]
+        [Tooltip("Handles audio functionalities like playing sounds and managing music.")]
+        [SerializeField] private AudioManager _audioManager;
+
+        [Tooltip("Manages particle effects used throughout the game.")]
+        [SerializeField] private ParticleManager _particleManager;
+
+        [Tooltip("Handles time management including countdowns, timers, and related functions.")]
+        [SerializeField] private TimeManager _timeManager;
+
+        /// <summary>
+        /// Provides public access to the AudioManager instance.
+        /// </summary>
+        public AudioManager AudioManager => _audioManager;
+
+        /// <summary>
+        /// Provides public access to the ParticleManager instance.
+        /// </summary>
+        public ParticleManager ParticleManager => _particleManager;
+
+        /// <summary>
+        /// Provides public access to the TimeManager instance.
+        /// </summary>
+        public TimeManager TimeManager => _timeManager;
+    }
+}
