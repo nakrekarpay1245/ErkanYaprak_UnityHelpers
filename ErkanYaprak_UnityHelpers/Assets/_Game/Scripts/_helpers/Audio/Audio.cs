24f0fbcc52e2789526cf26067cd207838f9fea35
nakrekarpay1245 <rknyprk2001@gmail.com>
2024-08-21 12:08:32 +0300

    feat: Added Audio Script
    Purpose:
    -Represents multiple audio clips (AudioClip) and includes settings for volume, pitch, and looping for each sound.
    
    Features:
    -Contains a name (Name) for the audio and an array of audio clips (_clipArray).
    -Provides functionality to select and return a random audio clip (Clip).
    -Includes properties for volume (Volume), pitch (Pitch), and whether the audio should loop (Loop).

---
+++ ErkanYaprak_UnityHelpers/Assets/_Game/Scripts/_helpers/Audio/Audio.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+namespace _Game.Scripts._helpers
+{
+    /// <summary>
+    /// Represents an audio configuration with multiple clips, volume, pitch, and loop settings.
+    /// </summary>
+    [System.Serializable]
+    public class Audio
+    {
+        [Header("Audio Settings")]
+        [Tooltip("The name of the audio.")]
+        public string Name;
+
+        [Tooltip("Array of audio clips that can be played.")]
+        [SerializeField]
+        private AudioClip[] _clipArray;
+
+        /// <summary>
+        /// Gets a random clip from the audio clip array.
+        /// </summary>
+        public AudioClip Clip => _clipArray[Random.Range(0, _clipArray.Length)];
+
+        [Range(0f, 1f)]
+        [Tooltip("Volume level for the audio.")]
+        public float Volume = 1f;
+
+        [Range(0.1f, 3f)]
+        [Tooltip("Pitch level for the audio.")]
+        public float Pitch = 1f;
+
+        [Tooltip("Should the audio loop?")]
+        public bool Loop = false;
+    }
+}
